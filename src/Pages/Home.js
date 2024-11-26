21b2ab60fbe8785c13585ab911273246d6028629
mhojune <mhojune@gmail.com>
2024-11-26 09:52:26 +0900

    feature: add searchpage and bookmarkpage Can search word and bookmark. In Bookmarkpage, you can see bookmarked word and goto word's search page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    return <div className="home"></div>;
+    const [word, setWord] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const trimmedWord = word.trim();
+        if (!trimmedWord || trimmedWord.split(" ").length > 1) return;
+        navigate(`/search/${word}`);
+    };
+
+    return (
+        <div className="home">
+            <h1 className="Title">Dictionary</h1>
+            <p className="addi">Find meanings and save for quick reference</p>
+            <form onSubmit={handleSubmit} className="searchbar">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <input
+                    className="search"
+                    value={word}
+                    onChange={(e) => setWord(e.target.value)}
+                    placeholder="Type to search...."
+                />
+            </form>
+        </div>
+    );
 };
 
 export default Home;
